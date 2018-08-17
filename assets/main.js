d32a2a72520b7760dd6575c0622fa0311a07bf35
Alexey Stepankov <xeurun@yahoo.com>
2018-08-18 01:56:37 +0400

    Add sharable link feature

--- assets/main.js
+++ assets/main.js
@@ -3,6 +3,7 @@
         let xmlhttp = new XMLHttpRequest();
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
+                const activeElementHash = document.location.hash.replace('#', '');
                 const data = JSON.parse(this.responseText);
                 const column = data.grid.column;
                 const row = data.grid.row;
@@ -28,16 +29,33 @@
                             symbolNode.innerText = cell.symbol;
                             let detailsNode = document.createElement('div');
                             detailsNode.className = 'at_details';
-                            // todo change 1 to random ?.??? digit
                             detailsNode.innerText = cell.title;
                             detailsNode.appendChild(document.createElement('br'));
                             weightNode = document.createElement('span');
                             weightNode.className = 'at_weight';
                             weightNode.innerText = cell.weight;
+                            if (activeElementHash !== undefined && activeElementHash === cell.symbol) {
+                                elementNode.className += ' active';
+                            }
                             detailsNode.appendChild(weightNode);
                             elementNode.appendChild(numNode);
                             elementNode.appendChild(symbolNode);
                             elementNode.appendChild(detailsNode);
+                            cellNode.addEventListener('click', function (event, tt) {
+                                document.location.hash = cell.symbol;
+                                let target = event.target || event.currentTarget;
+
+                                if (target.firstChild.className.indexOf('active') > 0) {
+                                    target.firstChild.className = target.firstChild.className.replace('active', '');
+                                } else {
+                                    var activeElement = document.getElementsByClassName('element active')[0];
+                                    if (activeElement !== undefined) {
+                                        activeElement.className = activeElement.className.replace('active', '');
+                                    }
+                                    target.firstChild.className = target.firstChild.className + ' ' + 'active';
+                                }
+
+                            });
                             cellNode.appendChild(elementNode);
                         }
 
